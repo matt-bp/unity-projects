e4d8bcad54b67c9e20eee978e1591001efac8796
Matt Bishop <mattsbishop1@gmail.com>
2022-11-28 15:42:42 -0700

    Working through scripting concepts

---
+++ Unity In Action - Chapter 2 Project/Assets/MouseLook.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public enum RotationAxes
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityHor = 9.0f;
+    public float sensitivityVert = 9.0f;
+
+    public float minVert = -45.0f;
+    public float maxVert = 45.0f;
+
+    private float verticalRot = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (axes == RotationAxes.MouseX)
+        {
+            
+            // horizontal rotation here
+            var speed = Input.GetAxis("Mouse X") * sensitivityHor;
+            transform.Rotate(0, speed, 0);
+        }
+        else if (axes == RotationAxes.MouseY)
+        {
+            // vertical rotation here
+            verticalRot -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            verticalRot = Mathf.Clamp(verticalRot, minVert, maxVert);
+
+            var horizontalRot = transform.localEulerAngles.y;
+
+            transform.localEulerAngles = new Vector3(verticalRot, horizontalRot, 0);
+        }
+        else
+        {
+            // both horizontal and vertical rotation here
+        }
+    }
+}
