7b224f45329167f8a86592b54aa46ecb98a008dd
Matt Bishop <mattsbishop1@gmail.com>
2023-03-25 13:44:47 -0600

    With now force, no acceleration is being applied to the cloth.

--- Cloth Simulation - 1D/Assets/Scripts/MassSpringCloth.cs
+++ Cloth Simulation - 1D/Assets/Scripts/MassSpringCloth.cs
@@ -1,37 +1,48 @@
 using UnityEngine;
 using System;
 using System.Linq;
+using Unity.VisualScripting;
 
 public class MassSpringCloth : MonoBehaviour
 {
     #region Editor Fields
     
     [SerializeField] private GameObject notification;
+    [SerializeField] private GameObject redNotification;
     
     #endregion
 
     private Mesh _mesh;
     private Vector3[] _positions;
-    private Vector3[] _forces;
-    private Vector3[] _velocities;
+    // private Vector3 _position0;
+    // private Vector3[] _forces;
+    private float _force0y;
+    // private Vector3[] _velocities;
+    private float _velocity0y;
 
+    private GameObject _vertex0Hint;
+    private GameObject _vertex1Hint;
+    
     #region Constants
 
     private readonly float gravity = 10;
     private readonly float k = 7;
     private readonly float restLength = 2.0f;
-    private readonly int[] masses = { 30, 0, 30, 0 };
+    private readonly int mass0 = 30;
 
     #endregion
-    
     
     private void Start()
     {
         _mesh = GetComponent<MeshFilter>().mesh;
         _positions = _mesh.vertices;
+        // _position0 = _positions[0];
+        
+        // _forces = FillEmpty(_positions.Length);
+        // _velocities = FillEmpty(_positions.Length);
 
-        _forces = FillEmpty(_positions.Length);
-        _velocities = FillEmpty(_positions.Length);
+        _force0y = 0;
+        _velocity0y = 0;
 
         // var temp = GetComponent<MeshFilter>();
         // var str = "";
@@ -51,62 +62,66 @@ public class MassSpringCloth : MonoBehaviour
         // temp.mesh.vertices = vertices;
         // temp.mesh.RecalculateBounds();
         //
-        Instantiate(notification, _positions[0], Quaternion.identity);
-        Instantiate(notification, _positions[2], Quaternion.identity);
+        _vertex0Hint = Instantiate(notification, _positions[0], Quaternion.identity);
+        _vertex1Hint = Instantiate(redNotification, _positions[2], Quaternion.identity);
     }
 
     private void Update()
     {
-        _positions[0].y += 0.5f * Time.deltaTime;
+        // Test moving mesh vertices
+        // _positions[0].y += 0.5f * Time.deltaTime;
 
+        SimulationStep();
+        
+        // Update mesh with new positions
         _mesh.vertices = _positions;
-        // SimulationStep();
-    }
+        _mesh.RecalculateBounds();
 
+
+        // Update hints
+        _vertex0Hint.transform.position = _positions[0];
+        _vertex1Hint.transform.position = _mesh.vertices[2];
+    }
+    
     private void SimulationStep()
     {
         // compute forces
         ComputeForces();
-
-        // for each particle
         
-        // update velocity
-
-        // update position
-
-        var a = _forces[0] / masses[0];
-
-        _velocities[0] += a * Time.deltaTime;
-
-        _positions[0] += _velocities[0] * Time.deltaTime;
-
+        var a = _force0y / mass0;
+    
+        _velocity0y += a * Time.deltaTime;
+    
+        _positions[0].y += _velocity0y * Time.deltaTime;
+    
         _mesh.vertices = _positions;
     }
-
+    
     private void ComputeForces()
     {
-        _forces = FillEmpty(_positions.Length);
-
-        _forces[0].y = masses[0] * gravity;
-        
-        var position1 = _positions[0];
-        var position2 = _positions[2];
-        
-        var velocity1 = _velocities[0];
-        var velocity2 = _velocities[2];
-        
-        var springForce = ComputeSpringForce(position1.y, position2.y);
-        var dampingForce = ComputeDampingForce(position1.y, position2.y, velocity1.y, velocity2.y);
-        
-        _forces[0].y += springForce + dampingForce;
+        _force0y = 0;
+        // _forces = FillEmpty(_positions.Length);
+        //
+        // _forces[0].y = mass0 * gravity;
+        //
+        // var position1 = _positions[0];
+        // var position2 = _positions[2];
+        //
+        // var velocity1 = _velocities[0];
+        // var velocity2 = _velocities[2];
+        //
+        // var springForce = ComputeSpringForce(position1.y, position2.y);
+        // var dampingForce = ComputeDampingForce(position1.y, position2.y, velocity1.y, velocity2.y);
+        //
+        // _forces[0].y += springForce + dampingForce;
     }
 
-    private Vector3[] FillEmpty(int count)
-    {
-        return Enumerable.Range(0, count)
-            .Select(i => new Vector3())
-            .ToArray();
-    }
+    // private Vector3[] FillEmpty(int count)
+    // {
+    //     return Enumerable.Range(0, count)
+    //         .Select(i => new Vector3())
+    //         .ToArray();
+    // }
 
     private float ComputeSpringForce(float pos1, float pos2)
     {
