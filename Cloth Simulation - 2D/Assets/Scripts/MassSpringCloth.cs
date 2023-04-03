5d5478a191af002cc298210c35fe3c9d64934591
Matt Bishop <mattsbishop1@gmail.com>
2023-04-03 09:07:04 -0600

    Refactored position update.

--- Cloth Simulation - 2D/Assets/Scripts/MassSpringCloth.cs
+++ Cloth Simulation - 2D/Assets/Scripts/MassSpringCloth.cs
@@ -21,14 +21,14 @@ public class MassSpringCloth : MonoBehaviour
     private GameObject[] _hints;
 
     #endregion
-    
+
     #region Simulation Constants
 
     private const int K = 7;
     private const float DampingCoef = 1.0f;
     private const float Gravity = -10.0f;
     private const int Mass = 1;
-    
+
     #endregion
 
     private void Start()
@@ -36,7 +36,7 @@ public class MassSpringCloth : MonoBehaviour
         _mesh = GetComponent<MeshFilter>().mesh;
         _positions = _mesh.vertices;
         _positions[0].x -= 2.0f;
-        
+
         _velocities = Enumerable.Range(0, _positions.Length).Select(_ => Vector2.zero).ToArray();
         _forces = Enumerable.Range(0, _positions.Length).Select(_ => Vector2.zero).ToArray();
 
@@ -52,7 +52,7 @@ public class MassSpringCloth : MonoBehaviour
         // Update mesh with new positions
         _mesh.vertices = _positions;
         _mesh.RecalculateBounds();
-        
+
         UpdateHints();
     }
 
@@ -61,11 +61,11 @@ public class MassSpringCloth : MonoBehaviour
     private void SimulationStep()
     {
         _forces = _forces.Select(x => Vector2.zero).ToArray();
-        
+
         // Compute forces
         var springForceY = -K * (_positions[0].y - _positions[2].y);
-        var springForceX = -K * (_positions[0].x - _positions[2].x); 
-        
+        var springForceX = -K * (_positions[0].x - _positions[2].x);
+
         var dampingForceY = DampingCoef * _velocities[0].y;
         var dampingForceX = DampingCoef * _velocities[0].x;
 
@@ -74,13 +74,14 @@ public class MassSpringCloth : MonoBehaviour
 
         var acceleration = new Vector2(_forces[0].x / Mass, _forces[0].y / Mass);
         _velocities[0] += acceleration * Time.deltaTime;
-
-        _positions[0].y += _velocities[0].y * Time.deltaTime;
-        _positions[0].x += _velocities[0].x * Time.deltaTime;
+        _positions[0] += new Vector3(
+            _velocities[0].x * Time.deltaTime,
+            _velocities[0].y * Time.deltaTime,
+            0);
     }
 
     #endregion
-    
+
     #region Vertex Hints
 
     private void AddHints()
@@ -90,7 +91,7 @@ public class MassSpringCloth : MonoBehaviour
             _hints[i] = Instantiate(vertexNotification, _positions[i], Quaternion.identity);
         }
     }
-    
+
     private void UpdateHints()
     {
         for (var i = 0; i < _hints.Length; i++)
@@ -98,6 +99,6 @@ public class MassSpringCloth : MonoBehaviour
             _hints[i].transform.position = _positions[i];
         }
     }
-    
+
     #endregion
 }
