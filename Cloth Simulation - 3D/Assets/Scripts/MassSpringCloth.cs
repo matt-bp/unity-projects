942e4ca53ac31d4788df7da77a02bed925e8a524
Matt Bishop <mattsbishop1@gmail.com>
2023-04-04 21:02:03 -0600

    Finished 3D mass spring cloth simulation.

--- Cloth Simulation - 3D/Assets/Scripts/MassSpringCloth.cs
+++ Cloth Simulation - 3D/Assets/Scripts/MassSpringCloth.cs
@@ -18,8 +18,8 @@ public class MassSpringCloth : MonoBehaviour
 
     private Mesh _mesh;
     private Vector3[] _positions;
-    private Vector2[] _velocities;
-    private Vector2[] _forces;
+    private Vector3[] _velocities;
+    private Vector3[] _forces;
     private GameObject[] _hints;
     private Dictionary<int, bool> _anchors = new();
 
@@ -27,7 +27,7 @@ public class MassSpringCloth : MonoBehaviour
 
     #region Simulation Constants
 
-    private const int K = 7;
+    private const int K = 4;
     private const float DampingCoef = 1.0f;
     private const float Gravity = -10.0f;
     private const float Mass = 1.5f;
@@ -40,12 +40,15 @@ public class MassSpringCloth : MonoBehaviour
         _mesh = GetComponent<MeshFilter>().mesh;
         _positions = _mesh.vertices;
         _positions[0].x -= 0.2f;
+        _positions[0].z += 0.5f;
+        
+        _positions[2].z -= 0.5f;
 
         _anchors[2] = true;
         _anchors[3] = true;
 
-        _velocities = Enumerable.Range(0, _positions.Length).Select(_ => Vector2.zero).ToArray();
-        _forces = Enumerable.Range(0, _positions.Length).Select(_ => Vector2.zero).ToArray();
+        _velocities = Enumerable.Range(0, _positions.Length).Select(_ => Vector3.zero).ToArray();
+        _forces = Enumerable.Range(0, _positions.Length).Select(_ => Vector3.zero).ToArray();
 
         _hints = new GameObject[_mesh.vertexCount];
 
@@ -75,12 +78,9 @@ public class MassSpringCloth : MonoBehaviour
         // Update velocities * positions
         for (var i = 0; i < _forces.Length; i++)
         {
-            var acceleration = new Vector2(_forces[i].x / Mass, _forces[i].y / Mass);
+            var acceleration = _forces[i] / Mass;
             _velocities[i] += acceleration * Time.deltaTime;
-            _positions[i] += new Vector3(
-                _velocities[i].x * Time.deltaTime,
-                _velocities[i].y * Time.deltaTime,
-                0);
+            _positions[i] += _velocities[i] * Time.deltaTime;
         }
     }
 
@@ -91,7 +91,7 @@ public class MassSpringCloth : MonoBehaviour
 
     private void ComputeForces()
     {
-        _forces = _forces.Select(x => Vector2.zero).ToArray();
+        _forces = _forces.Select(x => Vector3.zero).ToArray();
 
         // Set gravity contribution
         for (var i = 0; i < _forces.Length; i++)
@@ -108,9 +108,6 @@ public class MassSpringCloth : MonoBehaviour
             ComputeForceForPair(triangles[i + 1], triangles[i + 2]);
             ComputeForceForPair(triangles[i + 2], triangles[i]);
         }
-        
-        // This now works independent of the order of vertices
-        // ComputeForceForPair(2, 0);
     }
 
     private void ComputeForceForPair(int first, int second)
@@ -130,14 +127,14 @@ public class MassSpringCloth : MonoBehaviour
         }
     }
 
-    private Vector2 GetSpringForce(Vector3 position1, Vector3 position2)
+    private Vector3 GetSpringForce(Vector3 position1, Vector3 position2)
     {
         var distance = Vector3.Distance(position1, position2);
-        var force3 = K * (distance - RestLength) * ((position1 - position2) / distance);
-        return new Vector2(force3.x, force3.y);
+        var force = K * (distance - RestLength) * ((position1 - position2) / distance);
+        return force;
     }
 
-    private Vector2 GetDampingForce(Vector2 velocity1, Vector2 velocity2)
+    private Vector3 GetDampingForce(Vector3 velocity1, Vector3 velocity2)
     {
         return -DampingCoef * (velocity1 - velocity2);
     }
