c1f6b6f8933295eb488538defc92537d316b1e8d
Matt Bishop <mattsbishop1@gmail.com>
2023-04-08 15:28:02 -0600

    Started on intergrating VR and cloth.

---
+++ VR Cloth Simulation - Tracer Bullet/Assets/Scripts/VRButtonController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+
+public class VRButtonController : MonoBehaviour
+{
+    public void OnPrimaryButtonPressed()
+    {
+        Debug.Log("Pressed primary button");
+    }
+
+    public void OnSecondaryButtonPressed()
+    {
+        Debug.Log("Pressed secondary button");
+    }
+}
