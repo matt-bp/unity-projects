23621bc4a48fca11d385e5b7ccae2bc3034380fd
Matt Bishop <mattsbishop1@gmail.com>
2023-04-26 22:10:09 -0600

    Adding demo picture of project 6, and starting on mouse detection.

---
+++ Unity In Action - Chapter 6 Project/README.md
@@ -0,0 +1,5 @@
+# Chapter 6 Project - Platformer
+
+Focuses on platformer basics, like jumping, camera movement, and moving platforms. 
+
+![demo](./docs/demo.png)
---
+++ VR Cloth Simulation - Prototyping/Assets/Scripts/MouseDetection/TrackMouse.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace MouseDetection
+{
+    public class TrackMouse : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Sup");
+        }
+    }
+}
