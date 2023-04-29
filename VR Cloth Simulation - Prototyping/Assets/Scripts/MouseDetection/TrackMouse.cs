fa5f349bfa2d8dda3d9338496a42ad22eb898cd0
Matt Bishop <mattsbishop1@gmail.com>
2023-04-29 16:34:42 -0600

    Got mouse tracking working.

--- VR Cloth Simulation - Prototyping/Assets/Scripts/MouseDetection/TrackMouse.cs
+++ VR Cloth Simulation - Prototyping/Assets/Scripts/MouseDetection/TrackMouse.cs
@@ -1,13 +1,32 @@
 using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace MouseDetection
 {
     public class TrackMouse : MonoBehaviour
     {
+        [SerializeField] private Camera Camera;
+        
         private void Start()
         {
             Debug.Log("Sup");
+        }
+
+        private void Update()
+        {
+            var mousePos = Input.mousePosition;
+            var ray = Camera.ScreenPointToRay(mousePos);
+
+            if (Physics.Raycast(ray, out var hit))
+            {
+                Debug.Log("Found something!");  
+                // TODO: See if we're on a grab point, if so, change its color
+            }
+            else
+            {
+                Debug.Log("Not over something");
+            }
         }
     }
 }
