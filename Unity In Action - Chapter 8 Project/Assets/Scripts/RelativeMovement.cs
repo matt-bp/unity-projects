04641ce57b1b7c721c7ae8a4c887c19bcfba1599
Matt Bishop <mattsbishop1@gmail.com>
2023-05-08 22:37:29 -0600

    Working through chapter 8

--- Unity In Action - Chapter 8 Project/Assets/Scripts/RelativeMovement.cs
+++ Unity In Action - Chapter 8 Project/Assets/Scripts/RelativeMovement.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
+[RequireComponent(typeof(CharacterController))]
 public class RelativeMovement : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    private CharacterController _characterController;
+
+    public float moveSpeed = 6.0f; 
+    public float rotSpeed = 15.0f;
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
     
     void Update()
     {
@@ -19,9 +30,15 @@ public class RelativeMovement : MonoBehaviour
             // The camera is facing the ground, we don't want that forward vector
             var forward = Vector3.Cross(Vector3.right, Vector3.up);
             movement = (right * horInput) + (forward * vertInput);
+            movement *= moveSpeed;
+            movement = Vector3.ClampMagnitude(movement, moveSpeed);
 
             // Calculated a quaternion facing in the given direction
-            transform.rotation = Quaternion.LookRotation(movement);
+            var direction = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotSpeed * Time.deltaTime);
         }
+
+        movement *= Time.deltaTime;
+        _characterController.Move(movement);
     }
 }
