281312eb81ac136251c76bb7f3103f31c582d562
Matt Bishop <mattsbishop1@gmail.com>
2023-05-17 22:49:43 -0600

    Got getting a point into local space working.

---
+++ VR Cloth Simulation - Prototyping/Assets/Prototypes/05_Quad_Follow_Controller/Scripts/FirstVertexFollowController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Prototypes._05_Quad_Follow_Controller.Scripts
+{
+    [RequireComponent(typeof(MeshFilter))]
+    public class FirstVertexFollowController : MonoBehaviour
+    {
+        public InputActionReference controllerPositionAction;
+
+        [SerializeField]
+        private GameObject cameraFloorOffsetObject;
+        private MeshFilter meshFilter;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            meshFilter = GetComponent<MeshFilter>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var action = controllerPositionAction.action;
+
+            var position = action.ReadValue<Vector3>();
+            var offset = cameraFloorOffsetObject.transform.position.y;
+            position.y += offset;
+
+            var mesh = meshFilter.mesh;
+            // Local coordinate space
+            var newVertices = mesh.vertices;
+            
+            // How do I move the position into local space?
+            newVertices[0] = transform.InverseTransformPoint(position);
+
+            mesh.vertices = newVertices;
+            mesh.RecalculateBounds();
+        }
+    }
+}
