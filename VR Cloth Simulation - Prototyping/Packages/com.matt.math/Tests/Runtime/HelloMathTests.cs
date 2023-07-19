a76f7726d29073aa4d09076b330439d8e67d5f26
Matt Bishop <mattsbishop1@gmail.com>
2023-07-19 10:58:31 -0600

    Got the unit test for the math package working.

---
+++ VR Cloth Simulation - Prototyping/Packages/com.matt.math/Tests/Runtime/HelloMathTests.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+using UnityEngine.Assertions;
+using UnityEngine.TestTools;
+using Assert = NUnit.Framework.Assert;
+
+namespace Tests.Runtime
+{
+    public class HelloMathTests
+    {
+        [Test]
+        public void Test()
+        {  
+            Assert.Fail();
+        }
+    }
+}
