a4eb174f040b40ebe9eff8f801009d94d435027d
Matt Bishop <mattsbishop1@gmail.com>
2023-07-19 22:28:01 -0600

    Removing old unit tests, and working through copying over unit tests from the examples in previous prototypes.

--- VR Cloth Simulation - Prototyping/Packages/com.matt.math/Runtime/MattMath/ConjugateGradient1D.cs
+++ VR Cloth Simulation - Prototyping/Packages/com.matt.math/Runtime/MattMath/ConjugateGradient1D.cs
@@ -8,13 +8,13 @@ using Unity.Mathematics;
 
 namespace MattMath
 {
-    public class ConjugateGradient1D
+    public static class ConjugateGradient1D
     {
         public static GridVector CgAdd(GridVector first, GridVector second)
         {
             Debug.Assert(first.Count == second.Count);
 
-            GridVector result = Enumerable.Range(0, first.Count).Select(_ => 0.0).ToList();
+            GridVector result = first.Select(x => 0.0).ToList();
 
             for (var row = 0; row < first.Count; row++)
             {
@@ -22,6 +22,60 @@ namespace MattMath
             }
 
             return result;
+        }
+
+        public static GridVector CgSub(GridVector firstMatrix, GridVector secondMatrix)
+        {
+            Debug.Assert(firstMatrix.Count == secondMatrix.Count);
+
+            var result = firstMatrix.Select(x => 0.0).ToList();
+
+            for (var row = 0; row < firstMatrix.Count; row++)
+            {
+                result[row] = firstMatrix[row] - secondMatrix[row];
+            }
+
+            return result;
+        }
+
+        public static GridVector CgMult(GridVector vector, double constant)
+        {
+            var result = vector.Select(x => x).ToList();
+
+            for (var row = 0; row < vector.Count; row++)
+            {
+                result[row] *= constant;
+            }
+
+            return result;
+        }
+
+        public static GridVector CgMult(GridMatrix matrix, GridVector vector)
+        {
+            Debug.Assert(vector.Count == matrix.Count);
+            var result = Enumerable
+                .Range(0, vector.Count)
+                .Select(_ => 0.0)
+                .ToList();
+
+            foreach (var (columnOfMatrices, columnIndex) in matrix.Select((v, i) => (v, i)))
+            {
+                Debug.Assert(columnOfMatrices.Count == matrix.Count);
+
+                foreach (var (cellMatrix, rowIndex) in columnOfMatrices.Select((cv, ci) => (cv, ci)))
+                {
+                    result[rowIndex] += cellMatrix * vector[columnIndex];
+                }
+            }
+
+            return result;
+        }
+
+        public static double CgDot(GridVector first, GridVector second)
+        {
+            Debug.Assert(first.Count == second.Count);
+
+            return first.Zip(second, (firstValue, secondValue) => firstValue * secondValue).Sum();
         }
     }
 }
--- VR Cloth Simulation - Prototyping/Packages/com.matt.math/Tests/Runtime/ConjugateGradient1DTests.cs
+++ VR Cloth Simulation - Prototyping/Packages/com.matt.math/Tests/Runtime/ConjugateGradient1DTests.cs
@@ -18,6 +18,42 @@ namespace Tests.Runtime
             AssertGridVectorsEqual(result, expected);
         }
         
+        [Test]
+        public void CgSub_WithValues_ReturnsSubtractedResult()
+        {
+            var expected = new List<double> { 2.0, 0.0 };
+            var first = new List<double> { 4.0, -3.0 };
+            var second = new List<double> { 2.0, -3.0 };
+
+            var result = ConjugateGradient1D.CgSub(first, second);
+
+            AssertGridVectorsEqual(result, expected);
+        }
+        
+        [Test]
+        public void CgDot_WithValues_ReturnsDotProduct()
+        {
+            const double expected = 5;
+            var first = new List<double> { 1.0, 3.0 };
+            var second = new List<double> { 2.0, 1.0 };
+
+            var result = ConjugateGradient1D.CgDot(first, second);
+
+            Assert.That(result, Is.EqualTo(expected).Within(0.01));
+        }
+        
+        [Test]
+        public void CgMult_With1DVectors_MultipliesCorrectly()
+        {
+            var expected = new List<double> { 2.0, 1.5 };
+            var first = new List<double> { 4.0, 3.0 };
+
+            var result = ConjugateGradient1D.CgMult(first, 0.5);
+
+            AssertGridVectorsEqual(result, expected);
+        }
+
+        
         #region Helpers
         
         private void AssertGridVectorsEqual(IReadOnlyList<double> result, IReadOnlyList<double> expected)
