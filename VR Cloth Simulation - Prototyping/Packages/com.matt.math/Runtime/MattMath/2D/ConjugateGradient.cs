ce752fc44238f62772e1496d29b7fc905c9164d0
Matt Bishop <mattsbishop1@gmail.com>
2023-07-27 20:47:30 -0600

    Got first unit tests in for 2D conjugate gradient.

--- VR Cloth Simulation - Prototyping/Packages/com.matt.math/Runtime/MattMath/2D/ConjugateGradient.cs
+++ VR Cloth Simulation - Prototyping/Packages/com.matt.math/Runtime/MattMath/2D/ConjugateGradient.cs
@@ -1,7 +1,24 @@
+using System.Linq;
+using UnityEngine;
+using GridMatrix = System.Collections.Generic.List<System.Collections.Generic.List<Unity.Mathematics.double2x2>>;
+using GridVector = System.Collections.Generic.List<Unity.Mathematics.double2>;
+
 namespace MattMath._2D
 {
     public class ConjugateGradient
     {
-        
+        public static GridVector CgAdd(GridVector first, GridVector second)
+        {
+            Debug.Assert(first.Count == second.Count);
+
+            var result = first.Select(x => x.xy).ToList();
+
+            for (var row = 0; row < first.Count; row++)
+            {
+                result[row] = first[row] + second[row];
+            }
+
+            return result;
+        }
     }
 }
---
+++ VR Cloth Simulation - Prototyping/Packages/com.matt.math/Tests/Runtime/2D/ConjugateGradient2DTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using MattMath;
+using MattMath._2D;
+using NUnit.Framework;
+using UnityEngine;
+using Unity.Mathematics;
+
+namespace Tests.Runtime._2D
+{
+    public class ConjugateGradient2DTests
+    {
+        [Test]
+        public void CgAdd_WithValues_AddsThemTogether()
+        {
+            var expected = MakeExpected();
+            var first = MakeFirst();
+            var second = MakeSecond();
+
+            var result = ConjugateGradient.CgAdd(first, second);
+
+            AssertGridVectorsEqual(result, expected);
+        }
+        
+        #region Helpers
+
+        private static List<double2> MakeExpected() => new()
+        {
+            math.double2(7.0, 5),
+            math.double2(1.0, 3.0)
+        };
+        
+        private static List<double2> MakeFirst() => new()
+        {
+            math.double2(4.0, 3.5),
+            math.double2(1.0, 2.0)
+        };
+        
+        private static List<double2> MakeSecond() => new()
+        {
+            math.double2(3.0, 1.5),
+            math.double2(0.0, 1.0)
+        };
+
+        private static void AssertGridVectorsEqual(List<double2> result, List<double2> expected)
+        {
+            Assert.That(result, Has.Count.EqualTo(expected.Count));
+
+            for (var i = 0; i < expected.Count; i++)
+            {
+                Assert.That(result[i], Is.EqualTo(expected[i]));
+            }
+        }
+
+        #endregion
+    }
+}
