ae78df6d20b06528b4c0dfc0edc0e208cf6c1325
Matt Bishop <mattsbishop1@gmail.com>
2023-08-22 21:34:41 -0600

    Added double3 outer product tests.

---
+++ VR Cloth Simulation - Project Pikachu/Packages/com.pika.math/UnitTests/Runtime/LinearAlgebra/Double3Tests.cs
@@ -0,0 +1,21 @@
+﻿using LinearAlgebra;
+using NUnit.Framework;
+using Unity.Mathematics;
+
+namespace UnitTests.Runtime.LinearAlgebra
+{
+    public class Double3Tests
+    {
+        [Test]
+        public void OuterProduct_WithNonZeroVectors_ReturnsOuterProduct()
+        {
+            var column = math.double3(2, 4, 6);
+            var row = math.double3(3, 5, 7);
+            var expected = math.double3x3(6, 10, 14,  12, 20, 28,  18, 30, 42);
+
+            var result = Double3.OuterProduct(column, row);
+            
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
