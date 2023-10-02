68e6fd26f7fc406b21ee38e68466cf0fc0e9f1b1
Matt Bishop <mattsbishop1@gmail.com>
2023-10-02 09:25:57 -0600

    Getting started on the shear condition.

---
+++ VR Cloth Simulation - Project Pikachu/Packages/com.pika.continuum-cloth/Pika.Continuum.Cloth.UnitTests/Conditions/ShearConditionQuantitiesTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Conditions;
+using NSubstitute;
+using NUnit.Framework;
+using Triangles;
+using Unity.Mathematics;
+
+namespace Pika.Continuum.Cloth.UnitTests.Conditions
+{
+    public class ShearConditionQuantitiesTests
+    {
+        [Test]
+        public void C_OnRest_ReturnsZero()
+        {
+            var stubCombined = Substitute.For<ICombinedTriangle>();
+            stubCombined.A.Returns(0.625);
+            stubCombined.Wu.Returns(math.double3(1, 0, 1));
+            stubCombined.Wv.Returns(math.double3(1, 0, 1));
+            var stretchQuantities = new ShearConditionQuantities(stubCombined);
+
+            var result = stretchQuantities.C;
+            
+            Assert.That(result, Is.EqualTo(1.25));
+        }
+    }
+}
