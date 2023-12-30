b6dca614114748b04ffffe011a3012d0c994aef6
Matt Bishop <mattsbishop1@gmail.com>
2023-12-30 11:11:56 -0700

    Got quad appearing.

--- rendering/Assets/Projects/Procedural_Grid/Scripts/Grid.cs
+++ rendering/Assets/Projects/Procedural_Grid/Scripts/Grid.cs
@@ -8,6 +8,7 @@ public class Grid : MonoBehaviour
 {
     public int xSize, ySize;
     private Vector3[] vertices;
+    private Mesh mesh;
     
     private void Awake()
     {
@@ -17,17 +18,32 @@ public class Grid : MonoBehaviour
     private IEnumerator Generate()
     {
         var wait = new WaitForSeconds(0.05f);
+
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
         
         vertices = new Vector3[(xSize + 1) * (ySize + 1)];
         
         for (int i = 0, y = 0; y <= ySize; y++)
         {
-            for (var x = 0; x < xSize; x++, i++)
+            for (var x = 0; x <= xSize; x++, i++)
             {
                 vertices[i] = new Vector3(x, y);
                 yield return wait;
             }
         }
+        
+        mesh.vertices = vertices;
+
+        var triangles = new int[6];
+        triangles[0] = 0;
+        triangles[1] = xSize + 1;
+        triangles[2] = 1;
+        triangles[3] = 1;
+        triangles[4] = xSize + 1;
+        triangles[5] = xSize + 2;
+
+        mesh.triangles = triangles; // has to come after setting vertices
     }
 
     private void OnDrawGizmos()
