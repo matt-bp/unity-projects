73daa1a2d5d47f474b5b424cc92353548c1c69bb
Matt Bishop <mattsbishop1@gmail.com>
2023-12-30 11:03:00 -0700

    Started with procedural grid.

---
+++ rendering/Assets/Projects/Procedural_Grid/Scripts/Grid.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Grid : MonoBehaviour
+{
+    public int xSize, ySize;
+    private Vector3[] vertices;
+    
+    private void Awake()
+    {
+        StartCoroutine(Generate());
+    }
+
+    private IEnumerator Generate()
+    {
+        var wait = new WaitForSeconds(0.05f);
+        
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for (var x = 0; x < xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y);
+                yield return wait;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices is null)
+        {
+            return;
+        }
+        
+        Gizmos.color = Color.black;
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            // Drawing in world space
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+}
